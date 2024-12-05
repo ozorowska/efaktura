16e737549c0c27858dfb3d897673fa4c539192a5
Agata <agata.ozorowska.123@gmail.com>
2024-12-05 19:11:08 +0100

    Konfiguracja bazy danych i narzędzi autoryzacji

---
+++ app/login/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signIn } from "../utils/auth";
+
+export default function Login(){
+    return(
+       <>
+        
+        <div className= "flex h-screen w-full items-center justify-center px-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">
+                        Logowanie
+                    </CardTitle>
+                    <CardDescription>
+                        Zaloguj się do swojego konta wpisując swój adres e-mail poniżej
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form action={async () => {
+                     "use server"
+                    await signIn() }}
+                     className="flex flex-col gap-y-4">
+                        <div className="flex flex-col gap-y-2">
+                        <Label>E-mail</Label>
+                            <Input placeholder="uzykowink@email.com"></Input>
+                        
+                        </div>
+                        <div>
+                        <Button>Zaloguj</Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+        </>);
+    }
