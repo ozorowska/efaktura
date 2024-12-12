4543cbc537251757929defecd8342a5374645d0a
Agata <agata.ozorowska.123@gmail.com>
2024-12-12 13:15:14 +0100

    Dodanie przycisku do wylogowania

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import Logo from '@/public/moneylogo.svg'
 import Image from "next/image";
 import { DashboardLinks } from "@/components/DashboardLinks";
-import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { CircleUserRound, ContactRound, Menu, User, User2, UserPlus2, UserX2 } from "lucide-react";
+import { DropdownMenu, DropdownMenuLabel, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { signOut } from "../utils/auth";
 
 
 export default async function DashboardLayout({children}:{children: ReactNode}) 
@@ -34,7 +36,58 @@ export default async function DashboardLayout({children}:{children: ReactNode})
                  </div>
 
                  <div className="flex flex-col">
-                   
+                    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 ">
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button variant="outline" size="icon" className="md:hidden">
+                                    <Menu className="size-5"/>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side="left">
+                                <nav className="grid gap-2 mt-10">
+                                    <DashboardLinks />
+                                </nav>
+                            </SheetContent>
+                        </Sheet>
+                        <div className="flex items-center ml-auto">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button
+                                    className="rounded-full"
+                                    variant="ghost"
+                                    size="icon"
+                                    >
+                                        <User/>
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end">
+                                    <DropdownMenuLabel>
+                                        Moje konto
+                                    </DropdownMenuLabel>
+                                    <DropdownMenuSeparator/>
+                                    <DropdownMenuItem asChild>
+                                        <Link href="/dashboard">Panel zarządzanai</Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <Link href="/dashboard/invoices">Faktury </Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <form action={async () => {
+                                            "use server"
+                                            await signOut()
+                                        }}>
+                                            <button>
+                                                Wyloguj
+                                            </button>
+                                        </form>
+                                    </DropdownMenuItem>
+
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                            
+                        </div>
+
+                    </header>
 
                  </div>
             </div>
